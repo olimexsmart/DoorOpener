ce0fd4f612814ef343935619927089ca48c1afa6
Luca Olivieri <gliolivieri@live.it>
2018-02-25 19:01:37 +0100

    Loaded page

---
+++ README.md
@@ -0,0 +1,9 @@
+# DoorOpener
+I have:
++ an Arduino with Ethernet connection
++ a SD card to store the web pages on
++ a door to open
++ some spare time and glory to achieve
+
+vabbuo.it
+olimexsmart.it
--- poiboh.ino
+++ poiboh.ino
@@ -1,65 +1,94 @@
 /*
-  SD card file dump
+  Server Door Opener
 
- This example shows how to read a file from the SD card using the
- SD library and send it over the serial port.
-
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
- created  22 December 2010
- by Limor Fried
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
+*/
 
 #include <SPI.h>
+#include <Ethernet.h>
 #include <SD.h>
 
-const int chipSelect = 4;
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+#define chipSelect 4
+#define led 2
 
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 33); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
 
-  Serial.print("Initializing SD card...");
+File webFile;
 
-  // see if the card is present and can be initialized:
+char index[] = "index.htm";
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
+  Ethernet.begin(mac, ip);  // initialize Ethernet device
+  server.begin();           // start to listen for clients
+  Serial.begin(9600);       // for debugging
+
+  // initialize SD card
+  Serial.println("Initializing SD card...");
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
   }
-  Serial.println("card initialized.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("Inferno.txt");
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    while (dataFile.available()) {
-      Serial.write(dataFile.read());
+  Serial.println("SUCCESS - SD card initialized.");
+  //check for index.htm file
+  if (!SD.exists(index)) {
+      Serial.println("ERROR - Can't find index.htm file!");
+      return;  // can't find index file
     }
-    dataFile.close();
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
+  Serial.println("SUCCESS - Found index.htm file.");
+
 }
 
-void loop() {
+void loop()
+{
+  EthernetClient client = server.available();  // try to get client
+
+  if (client) {  // got client?
+    digitalWrite(led, HIGH);
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {   // client data available to read
+        char c = client.read(); // read 1 byte (character) from client
+        Serial.print(c);
+        // last line of client request is blank and ends with \n
+        // respond to client only after last line received
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          // send web page
+          webFile = SD.open(index);        // open web page file
+          if (webFile) {
+            while (webFile.available()) {
+              client.write(webFile.read()); // send web page to client
+            }
+            webFile.close();
+          }
+          break;
+        }
+        // every line of text received from the client ends with \r\n
+        if (c == '\n') {
+          // last character on line of received text
+          // starting new line with next character read
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // a text character was received from client
+          currentLineIsBlank = false;
+        }
+      } // end if (client.available())
+    } // end while (client.connected())
+    delay(1);      // give the web browser time to receive the data
+    client.stop(); // close the connection
+    digitalWrite(led, LOW);
+  } // end if (client)
 }
 
