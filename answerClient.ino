1fd08c7551845264f9965f5e58682ab38977e09e
Luca Olivieri <gliolivieri@live.it>
2018-03-02 22:50:47 +0100

    Saving memory F() and pefected response headers

--- answerClient.ino
+++ answerClient.ino
@@ -14,27 +14,27 @@ void answerClient(EthernetClient & client, HTTPparser & parser) {
             // Check if we have the file
             if (SD.exists(parser.Path)) {
                 webFile = SD.open(parser.Path);   // open web page file
-            } else {
-            	strcpy(parser.Path, "404.htm");
-                webFile = SD.open("404.htm");   // 404 landing page                
-            }
-
-            // Detect format
-            {   // Scoping brackets since in switch statements these declarations
-                // would pollute other cases
-                char * point = strrchr(parser.Path, '.');
-                char format[5];
-                strcpy(format, point + 1);
-                // Here there should be a general mapping of format-MIMI types
-                if (strcmp(format, "htm") == 0) {
-                    sendHeaders(client, "text/html");
-                } else if (strcmp(format, "css") == 0) {
-                    sendHeaders(client, "text/css");
-                } else if (strcmp(format, "ico") == 0) {
-                    sendHeaders(client, "image/x-icon");
-                } else {
-                    sendHeaders(client, "text/plain");
+                // Detect format
+                {   // Scoping brackets since in switch statements these declarations
+                    // would pollute other cases
+                    char * point = strrchr(parser.Path, '.');
+                    char format[5];
+                    strcpy(format, point + 1);
+                    // Here there should be a general mapping of format-MIMI types
+                    if (strcmp(format, "htm") == 0) {
+                        sendHeaders(200, client, "text/html");
+                    } else if (strcmp(format, "css") == 0) {
+                        sendHeaders(200, client, "text/css");
+                    } else if (strcmp(format, "ico") == 0) {
+                        sendHeaders(200, client, "image/x-icon");
+                    } else {
+                        sendHeaders(200, client, "text/plain");
+                    }
                 }
+            } else {	// 404 landing page
+                strcpy(parser.Path, "404.htm");
+                webFile = SD.open("404.htm");
+                sendHeaders(404, client, "text/html");
             }
             // Send the actual file
             while (webFile.available()) {
@@ -43,28 +43,58 @@ void answerClient(EthernetClient & client, HTTPparser & parser) {
             webFile.close();
             break;
 
-        case HTTPparser::POST:
-            sendHeaders(client);
+        case HTTPparser::POST: // Here the code will be pretty specific of the application
+            if (strcmp(parser.Path, "/login.ard") == 0) {
+            	// Elaborate the data 
+				
+            	
+                sendHeaders(200, client, NULL);
+                // Send response				               
+            } else {
+                sendHeaders(418, client, NULL);
+            }
             break;
 
         default:
             // 501 not implemented
+            sendHeaders(501, client, NULL);
             break;
     }
 }
 
 
 // Constant arguments declared only in the prototype
-void sendHeaders(EthernetClient & client, const char * mime) {
+void sendHeaders(int code, EthernetClient & client, const char * mime) {
     // Sent some HTTP headers
-    client.println("HTTP/1.1 200 OK");
+    switch (code) {
+        case 200:
+            client.println(F("HTTP/1.1 200 OK"));
+            break;
+        case 404:
+            client.println(F("HTTP/1.1 404 Not Found"));
+            break;
+        case 418:
+            client.println(F("HTTP/1.1 418 I'm a teapot"));
+            break;
+        case 501:
+            client.println(F("HTTP/1.1 501 Not Implemented"));
+            break;
+        case 500:
+            client.println(F("HTTP/1.1 501 Internal Server Error"));
+            break;
+    }
+
     if (mime) {
         char buff[30] = "Content-Type: ";
         strcat(buff, mime); // Append correct image format
         client.println(buff);
     }
-    client.println("Connection: close");
+    client.println(F("Connection: close"));
     client.println();
+}
+
+bool checkValidity(char * credentials) {
+	//File credentials
 }
 
 
--- poiboh.ino
+++ poiboh.ino
@@ -16,7 +16,7 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 33); // IP address, may need to change depending on network
 EthernetServer server(80);  // create a server at port 80
-HTTPparser Parser(35, 100);
+HTTPparser Parser(20, 50);
 
 File webFile;
 
@@ -30,18 +30,18 @@ void setup()
     Serial.begin(9600);       // for debugging
 
     // initialize SD card
-    Serial.println("Initializing SD card...");
+    Serial.println(F("Initializing SD card..."));
     if (!SD.begin(chipSelect)) {
-        Serial.println("ERROR - SD card initialization failed!");
+        Serial.println(F("ERROR - SD card initialization failed!"));
         return;    // init failed
     }
-    Serial.println("SUCCESS - SD card initialized.");
+    Serial.println(F("SUCCESS - SD card initialized."));
     //check for index.htm file
     if (!SD.exists("index.htm")) {
-        Serial.println("ERROR - Can't find index.htm file!");
+        Serial.println(F("ERROR - Can't find index.htm file!"));
         return;  // can't find index file
     }
-    Serial.println("SUCCESS - Found index.htm file.");
+    Serial.println(F("SUCCESS - Found index.htm file."));
 
 }
 
@@ -51,7 +51,7 @@ void loop()
 
     if (client) {  // got client?
         digitalWrite(led, HIGH);
-			
+
         // Collecting data from client
         while (client.connected()) {
             if (client.available()) {   // client data available to read
@@ -66,16 +66,18 @@ void loop()
         // Tell the parser we are done
         Parser.AllSheWrote();
         if (Parser.IsValid()) { // Print some debug
-        	Serial.println();
+            Serial.println();
             Serial.println(Parser.MethodString());
             Serial.println(Parser.Path);
             Serial.println(Parser.Message);
-        } else
-            Serial.println("We have an error");
-
-        // Answer the client and elaborate actions
-        answerClient(client, Parser);
-
+            // Answer the client and elaborate actions
+            answerClient(client, Parser);
+        } else {
+            Serial.println(F("We have an error"));
+            // Error 500
+            sendHeaders(500, client, NULL);
+        }
+        
         delay(1);      // give the web browser time to receive the data
         client.stop(); // close the connection
         Parser.Reset(); // Prepare parser for new request
